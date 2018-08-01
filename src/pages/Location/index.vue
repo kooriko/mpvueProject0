c5ba42ce4213d6174c63d89851669475c0249316
杨文杰 <ywj@geekniu.com>
2018-08-02 07:21:36 +0800

    init styles

--- src/pages/Location/index.vue
+++ src/pages/Location/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container">
+    <div class="location-container">
         Location Page
     </div>
 </template>
@@ -41,32 +41,7 @@ export default {
 </script>
 
 <style scoped>
-.userinfo {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-}
+.container {
 
-.userinfo-avatar {
-    width: 128rpx;
-    height: 128rpx;
-    margin: 20rpx;
-    border-radius: 50%;
 }
-
-.userinfo-nickname {
-    color: #aaa;
-}
-
-.usermotto {
-    margin-top: 150px;
-}
-
-.form-control {
-    display: block;
-    padding: 0 12px;
-    margin-bottom: 5px;
-    border: 1px solid #ccc;
-}
-
 </style>
--- src/pages/index/main.js
+++ src/pages/index/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './index'
-import wxDom from '@/libs/wx-dom'
+// import wxDom from '@/libs/wx-dom'
 
 // wxDom(App)
 const app = new Vue(App)
